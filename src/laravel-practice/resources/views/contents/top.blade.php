a9606ff0c839028fe3969e4fabdee96b3f29e2d3
mokuharu <bakki.taku@gmail.com>
2022-02-06 00:31:24 +0900

    add top page

---
+++ src/laravel-practice/resources/views/contents/top.blade.php
@@ -0,0 +1,155 @@
+@extends('layouts.main')
+@section('title', 'Topページ')
+@section('content')
+
+<div class="container">
+  <div class="row align-items-center">
+
+    <div class="col">
+      <div class="shadow card my-3" style="min-width: 360px; max-width:540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="{{asset('images/img-tetsuya01.jpg')}}" class="img-fluid py-2 px-1 w-100 h-auto" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">てつや</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="shadow list-group-item">総罰ゲーム数</li>
+              <li class="shadow list-group-item">残罰ゲーム数</li>
+              <li class="shadow list-group-item">回避券</li>
+            </ul>
+            <div class="card-body">
+              <a href="#" class="btn btn-outline-primary">Card link</a>
+              <a href="#" class="btn btn-outline-primary">Another link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="shadow card my-3" style="min-width: 360px; max-width:540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="{{asset('images/img-shibayu01.jpg')}}" class="img-fluid py-2 px-1 w-100 h-auto" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">しばゆー</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="shadow list-group-item">総罰ゲーム数</li>
+              <li class="shadow list-group-item">残罰ゲーム数</li>
+              <li class="shadow list-group-item">回避券</li>
+            </ul>
+            <div class="card-body">
+              <a href="#" class="btn btn-outline-primary">Card link</a>
+              <a href="#" class="btn btn-outline-primary">Another link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="shadow card my-3" style="min-width: 360px; max-width:540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="{{asset('images/img-ryo01.jpg')}}" class="img-fluid py-2 px-1 w-100 h-auto" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">りょう</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="shadow list-group-item">総罰ゲーム数</li>
+              <li class="shadow list-group-item">残罰ゲーム数</li>
+              <li class="shadow list-group-item">回避券</li>
+            </ul>
+            <div class="card-body">
+              <a href="#" class="btn btn-outline-primary">Card link</a>
+              <a href="#" class="btn btn-outline-primary">Another link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="shadow card my-3" style="min-width: 360px; max-width:540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="{{asset('images/img-toshimitsu01.jpg')}}" class="img-fluid py-2 px-1 w-100 h-auto" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">としみつ</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="shadow list-group-item">総罰ゲーム数</li>
+              <li class="shadow list-group-item">残罰ゲーム数</li>
+              <li class="shadow list-group-item">回避券</li>
+            </ul>
+            <div class="card-body">
+              <a href="#" class="btn btn-outline-primary">Card link</a>
+              <a href="#" class="btn btn-outline-primary">Another link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="shadow card my-3" style="min-width: 360px; max-width:540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="{{asset('images/img-yumemaru01.jpg')}}" class="img-fluid py-2 px-1 w-100 h-auto" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">ゆめまる</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="shadow list-group-item">総罰ゲーム数</li>
+              <li class="shadow list-group-item">残罰ゲーム数</li>
+              <li class="shadow list-group-item">回避券</li>
+            </ul>
+            <div class="card-body">
+              <a href="#" class="btn btn-outline-primary">Card link</a>
+              <a href="#" class="btn btn-outline-primary">Another link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="shadow card my-3" style="min-width: 360px; max-width:540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="{{asset('images/img-mushimegane01.jpg')}}" class="img-fluid py-2 px-1 w-100 h-auto" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">虫眼鏡</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="shadow list-group-item">総罰ゲーム数</li>
+              <li class="shadow list-group-item">残罰ゲーム数</li>
+              <li class="shadow list-group-item">回避券</li>
+            </ul>
+            <div class="card-body">
+              <a href="#" class="btn btn-outline-primary">Card link</a>
+              <a href="#" class="btn btn-outline-primary">Another link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+@endsection
---
+++ src/laravel-practice/resources/views/layouts/main.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title')</title>
+
+  <link rel="stylesheet" href="/css/header.css">
+  <link rel="stylesheet" href="/css/footer.css">
+
+  <!--Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  <!--Font Awesome5-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+
+</head>
+
+<body class="bg-dark">
+  @include('layouts.header')
+
+  @yield('content')
+
+  @include('layouts.footer')
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+  <!-- ionicon -->
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
