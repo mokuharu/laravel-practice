85e51f1f76eec2e73b0e56b63e75fd89d3cd7144
mokuharu <bakki.taku@gmail.com>
2022-02-06 15:39:51 +0900

    add routing create page

--- src/laravel-practice/routes/web.php
+++ src/laravel-practice/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('contents.top');
 });
 
+Route::get('create', function(){
+    return view('contents.create');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
